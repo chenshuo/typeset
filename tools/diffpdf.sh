ff6201706a4854eda47c211eebcdc6bac778a062
Shuo Chen <chenshuo@chenshuo.com>
2012-12-19 11:48:15 +0800

    add diffpng.py

--- tools/diffpdf.sh
+++ tools/diffpdf.sh
@@ -4,7 +4,7 @@ OLDPDF=$1
 NEWPDF=$2
 
 if [ -e "$OLDPDF" -a -e "$NEWPDF" ]; then
-  mkdir new old
+  mkdir new old dif
 
   gs -dNOPAUSE -dBATCH -sDEVICE=pnggray \
     -r144 -dTextAlphaBits=4 -dUseCropBox -sOutputFile='old/page-%03d.png' $OLDPDF &
@@ -14,7 +14,7 @@ if [ -e "$OLDPDF" -a -e "$NEWPDF" ]; then
 
   wait %1
 
-  diff old new
+  diff old new | awk '/^Binary.*differ/{print "./diffpng.py", $3, $5, "xx"$5}'|sed 's/xxnew/dif/'
 else
   echo "Usage $0 oldpdf newpdf"
 fi
